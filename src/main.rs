0d855119a7483aee83f49add8ffc909428e5a9dc
Jos Dehaes <jos.dehaes@gmail.com>
2022-03-06 21:21:28 +0100

    option parsing + recursive off/on

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,26 @@
-use std::{env, path::Path};
-use clap::{arg, command, Command};
+use clap::{arg, command};
 
 use config::Config;
-use regex::Regex;
 
 mod search;
 mod config;
 
-fn parse_args() -> Config {
-    let args: Vec<String> = env::args().collect();
-    let expr = args.get(1).unwrap();
-    let path = args.get(2).unwrap();
-    Config { expression: expr, path: *path, recursive: false }
-}
-
 fn main() {
-    let mut cmd = clap::command!()
+    let mut cmd = command!()
         // Add the version arguments
-        .arg(arg!(--"set-ver" <VER> "set version manually").required(false))
-        .arg(arg!(--major         "auto inc major"))
-        .arg(arg!(--minor         "auto inc minor"))
-        .arg(arg!(--patch         "auto inc patch"))
-        // Arguments can also be added to a group individually, these two arguments
-        // are part of the "input" group which is not required
-        .arg(arg!([INPUT_FILE] "some regular input"))
-        .arg(arg!(--"spec-in" <SPEC_IN> "some special input argument").required(false))
-        // Now let's assume we have a -c [config] argument which requires one of
-        // (but **not** both) the "input" arguments
-        .arg(arg!(config: -c <CONFIG>).required(false));
+        .arg(arg!(-r --recursive).required(false))
+        .arg(arg!([EXPR] "regex to search for"))
+        .arg(arg!([INPUT] "input file"));
     let matches = cmd.get_matches_mut();
 
-    let config = parse_args();
-    let re = Regex::new(config.expression.as_str()).unwrap();
-    let p = Path::new(config.path.as_str());
-    let mut matches : Vec<String> = Vec::new();
-    if p.is_dir() {
-        search::parse_dir(&re, &p, &mut matches);
-    } else if p.is_file() {
-        search::parse_file(&re, config.path.as_str(), &mut matches);
-    }
+    let pstr = matches.value_of("EXPR").unwrap();
+    let config = Config {
+        expression : String::from(pstr),
+        path : String::from(matches.value_of("INPUT").unwrap()),
+        recursive : matches.is_present("recursive"),
+    };
+    
+    let matches = search::search(&config);
     for m in matches {
         println!("{:?}", m);
     }
--- src/search.rs
+++ src/search.rs
@@ -1,21 +1,36 @@
-use std::{env, fs::File, io::Read, path::Path};
+use std::{fs::File, io::Read, path::Path};
 use regex::Regex;
 
-pub fn parse_dir(re: &Regex, path: &Path, matches: &mut Vec<String>) {
+use crate::config::Config;
+
+pub fn search(config: &Config) -> Vec<String> {
+    let mut matches : Vec<String> = Vec::new();
+    let re = Regex::new(config.expression.as_str()).unwrap();
+    let p = Path::new(config.path.as_str());
+    if p.is_dir() {
+        parse_dir(config, &re, p, &mut matches);
+    } else if p.is_file() {
+        parse_file(&re, p, &mut matches);
+    }
+    matches
+
+}
+
+pub fn parse_dir(config: &Config, re: &Regex, path: &Path, matches: &mut Vec<String>) {
     for entry in path.read_dir().unwrap() {
         if let Ok(entry) = entry {
-            if entry.path().is_dir() {
-                parse_dir(re, &entry.path(), matches);
+            if entry.path().is_dir() && config.recursive {
+                parse_dir(config, re, &entry.path(), matches);
             } else if entry.path().is_file() {
                 let n = &entry.path();
-                parse_file(re, n.to_str().unwrap(), matches);
+                parse_file(re, n, matches);
             }
         }
     }
 }
 
-pub fn parse_file(re: &Regex, path: &str, matches: &mut Vec<String>) {
-    println!("{:?}", path);
+pub fn parse_file(re: &Regex, path: &Path, matches: &mut Vec<String>) {
+    println!("Checking {:?}", path);
     let file = File::open(path);
     if file.is_ok() {
         let mut file = file.unwrap();
@@ -23,7 +38,7 @@ pub fn parse_file(re: &Regex, path: &str, matches: &mut Vec<String>) {
         if file.read_to_string(&mut contents).is_ok() {
             for line in contents.lines() {
                 if re.is_match(line) {
-                    let m = format!("{} : {}", path, line);
+                    let m = format!("{} : {}", path.to_str().unwrap(), line);
                     matches.push(m);
                 }
             }
