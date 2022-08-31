f46121c27d4763c2b4e7fb5f2f29f0954e4bc193
Jos Dehaes <jos.dehaes@gmail.com>
2022-08-31 23:28:08 +0200

    clippy

--- src/main.rs
+++ src/main.rs
@@ -4,36 +4,44 @@ use clap::{arg, command};
 
 use config::Config;
 
-mod search;
 mod config;
+mod search;
 
 fn main() {
     let cmd = command!()
-        .arg(arg!(-r --recursive).required(false))
-        .arg(arg!(-i --ignorecase).required(false))
-        .arg(arg!(-v --invert).required(false))
-        .arg(arg!(-F --fixedstrings).required(false))
+        .arg(arg!(-r - -recursive).required(false))
+        .arg(arg!(-i - -ignorecase).required(false))
+        .arg(arg!(-v - -invert).required(false))
+        .arg(arg!(-F - -fixedstrings).required(false))
         .arg(arg!([EXPR] "regex to search for"))
         .arg(arg!([INPUT] "input file"));
     let matches = cmd.get_matches();
 
     let pstr = matches.value_of("EXPR").unwrap();
     let config = Config {
-        expression : String::from(pstr),
-        path : String::from(matches.value_of("INPUT").unwrap()),
-        recursive : matches.is_present("recursive"),
-        ignore_case : matches.is_present("ignorecase"),
-        fixed_strings : matches.is_present("fixedstrings"),
-        invert : matches.is_present("invert"),
+        expression: String::from(pstr),
+        path: String::from(matches.value_of("INPUT").unwrap()),
+        recursive: matches.is_present("recursive"),
+        ignore_case: matches.is_present("ignorecase"),
+        fixed_strings: matches.is_present("fixedstrings"),
+        invert: matches.is_present("invert"),
     };
-    
+
     let matches = search::search(&config);
-    let empty = matches.is_empty();
-    for m in matches {
-        println!("{:?}", m);
+    match matches {
+        Ok(matches) => {
+            let empty = matches.is_empty();
+            for m in matches {
+                println!("{:?}", m);
+            }
+            if empty {
+                exit(1);
+            }
+            exit(0);
+        }
+        Err(e) => {
+            println!("Error: {}", e);
+            exit(-1);
+        }
     }
-    if empty {
-        exit(1);
-    }
-    exit(0);
 }
--- src/search.rs
+++ src/search.rs
@@ -7,10 +7,10 @@ use std::{
 
 use crate::config::Config;
 
-pub fn search(config: &Config) -> Vec<String> {
+pub fn search(config: &Config) -> Result<Vec<String>, String> {
     let mut matches: Vec<String> = Vec::new();
 
-    let mut expr  = String::new();
+    let mut expr = String::new();
     if config.ignore_case {
         expr.push_str("(?i)");
     }
@@ -18,48 +18,60 @@ pub fn search(config: &Config) -> Vec<String> {
     let re = Regex::new(&expr).unwrap();
     let p = Path::new(config.path.as_str());
     if p.is_dir() {
-        parse_dir(config, &re, p, &mut matches);
+        parse_dir(config, &re, p, &mut matches)?;
     } else if p.is_file() {
-        parse_file(config, &re, p, &mut matches);
+        parse_file(config, &re, p, &mut matches)?;
     }
-    matches
+    Ok(matches)
 }
 
-fn parse_dir(config: &Config, re: &Regex, path: &Path, matches: &mut Vec<String>) {
-    for entry in path.read_dir().unwrap() {
-        if let Ok(entry) = entry {
-            if entry.path().is_dir() && config.recursive {
-                parse_dir(config, re, &entry.path(), matches);
-            } else if entry.path().is_file() {
-                let n = &entry.path();
-                parse_file(config, re, n, matches);
-            }
+fn parse_dir(
+    config: &Config,
+    re: &Regex,
+    path: &Path,
+    matches: &mut Vec<String>,
+) -> Result<(), String> {
+    for entry in (path.read_dir().map_err(|e| e.to_string())?).flatten() {
+        if entry.path().is_dir() && config.recursive {
+            parse_dir(config, re, &entry.path(), matches)?;
+        } else if entry.path().is_file() {
+            let n = &entry.path();
+            parse_file(config, re, n, matches)?;
         }
     }
+    Ok(())
 }
 
-fn parse_file(config: &Config, re: &Regex, path: &Path, matches: &mut Vec<String>) {
-    let file = File::open(path);
-    if file.is_ok() {
-        let file = file.unwrap();
-        let reader = BufReader::new(file);
-        for (index, line) in reader.lines().enumerate() {
-            if let Ok(line) = line {
-                match_line(config, line, path, index, matches, re);
-            }
+fn parse_file(
+    config: &Config,
+    re: &Regex,
+    path: &Path,
+    matches: &mut Vec<String>,
+) -> Result<(), String> {
+    let file = File::open(path).map_err(|e| e.to_string())?;
+    let reader = BufReader::new(file);
+    for (index, line) in reader.lines().enumerate() {
+        if let Ok(line) = line {
+            match_line(config, line, path, index, matches, re);
         }
     }
+    Ok(())
 }
 
-fn match_line(config: &Config, line: String, path: &Path, index: usize, matches: &mut Vec<String>, re: &Regex) {
+fn match_line(
+    config: &Config,
+    line: String,
+    path: &Path,
+    index: usize,
+    matches: &mut Vec<String>,
+    re: &Regex,
+) {
     if config.fixed_strings {
         if config.invert ^ line.contains(config.expression.as_str()) {
             add_match(path, index, line, matches);
         }
-    } else {
-        if config.invert ^ re.is_match(line.as_str()) {
-            add_match(path, index, line, matches);
-        }
+    } else if config.invert ^ re.is_match(line.as_str()) {
+        add_match(path, index, line, matches);
     }
 }
 
@@ -77,20 +89,34 @@ mod tests {
     #[test]
     fn test_fixed_strings() {
         let config = Config::new(String::from("needle"), String::from("/tmp/foo"));
-        let mut matches : Vec<String> = Vec::new();
+        let mut matches: Vec<String> = Vec::new();
         let re = Regex::new("needle").unwrap();
-        let p = Path::new(config.path.as_str());    
-        match_line(&config, String::from("this line should match needle\nthis one shouldn't"), &p, 1, &mut matches, &re);
+        let p = Path::new(config.path.as_str());
+        match_line(
+            &config,
+            String::from("this line should match needle\nthis one shouldn't"),
+            &p,
+            1,
+            &mut matches,
+            &re,
+        );
         assert_eq!(matches.len(), 1);
         assert_eq!(matches.get(0).unwrap(), "");
     }
     #[test]
     fn test_regex() {
         let config = Config::new(String::from("needle"), String::from("/tmp/foo"));
-        let mut matches : Vec<String> = Vec::new();
+        let mut matches: Vec<String> = Vec::new();
         let re = Regex::new("needle").unwrap();
-        let p = Path::new(config.path.as_str());    
-        match_line(&config, String::from("this line should match needle\nthis one shouldn't"), &p, 1, &mut matches, &re);
+        let p = Path::new(config.path.as_str());
+        match_line(
+            &config,
+            String::from("this line should match needle\nthis one shouldn't"),
+            &p,
+            1,
+            &mut matches,
+            &re,
+        );
         assert_eq!(matches.len(), 1);
     }
 }
